bc59dac89b0937b9098788ad425d5a7ffbdb2fec
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-05-31 12:32:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "25891183"))
 API_HASH = getenv("API_HASH", "36709c81d7609a81f86de931cbc87f3a")
-BOT_TOKEN = getenv("BOT_TOKEN", "7480167477:AAF7957itkjVj0VOoQApYh4DzLTTAaHJn0c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7358694327:AAFkgFORP5deJ3rU1Gu3P-XfLS6euI89fEM")
 OWNER_ID = int(getenv("OWNER_ID", "6750546542"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6750546542").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://fiona171593:tbGMvepmKQ8YNfJy@cluster0.5ccbrkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
