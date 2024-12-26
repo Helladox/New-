13cbb8c8fad766f168c33365289dfa9424cb358c
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-12-27 01:53:43 +0530

    Update careerwill.py

--- Extractor/modules/careerwill.py
+++ Extractor/modules/careerwill.py
@@ -1,60 +1,68 @@
 import os
 import requests
 import logging
+import threading
 import asyncio
 from pyrogram import filters
 from Extractor import app
 from config import SUDO_USERS
+
 
 ACCOUNT_ID = "6206459123001"
 BCOV_POLICY = "BCpkADawqM1474MvKwYlMRZNBPoqkJY-UWm7zE1U769d5r5kqTjG0v8L-THXuVZtdIQJpfMPB37L_VJQxTKeNeLO2Eac_yMywEgyV9GjFDQ2LTiT4FEiHhKAUvdbx9ku6fGnQKSMB8J5uIDd"
 bc_url = f"https://edge.api.brightcove.com/playback/v1/accounts/{ACCOUNT_ID}/videos/"
 bc_hdr = {"BCOV-POLICY": BCOV_POLICY}
 
+
+
 async def careerdl(app, message, headers, raw_text2, class_id, notes_id, prog, name):
     if '/' in name:
         name1 = name.replace("/", "")
     else:
         name1 = name
-    
     try:
         num_id = class_id.split('&')
-        output_text = ""
+        for x in range(0, len(num_id)):
+            id_text = num_id[x]
 
-        for id_text in num_id:
-            details_url = f"https://elearn.crwilladmin.com/api/v5/batch-detail/{raw_text2}?topicId={id_text}"
+            details_url = "https://elearn.crwilladmin.com/api/v5/batch-detail/" + raw_text2 + "?topicId=" + id_text
             response = requests.get(details_url, headers=headers)
             data = response.json()
 
             details_list = data.get("data", {}).get("class_list", {})
             batch_class = details_list.get("classes", [])
-            batch_class.reverse()
 
+            batch_class.reverse()
+            output_text = ""
             for data in batch_class:
                 try:
                     vid_id = str(data['id'])
                     lesson_name = data['lessonName']
                     lessonExt = data['lessonExt']
-                    url = f"https://elearn.crwilladmin.com/api/v5/class-detail/{vid_id}"
+                    url = "https://elearn.crwilladmin.com/api/v5/class-detail/" + vid_id
                     lessonUrl = requests.get(url, headers=headers).json().get('data', {}).get('class_detail', {}).get('lessonUrl', '')
 
                     if lessonExt == 'brightcove':
-                        token_url = "https://elearn.crwilladmin.com/api/v5/livestreamToken"
+                        url = "https://elearn.crwilladmin.com/api/v5/livestreamToken"
                         params = {
                             "base": "web",
                             "module": "batch",
                             "type": "brightcove",
                             "vid": vid_id
                         }
-                        response = requests.get(token_url, headers=headers, params=params)
+
+                        response = requests.get(url, headers=headers, params=params)
                         stoken = response.json().get("data", {}).get("token", '')
-                        link = f"{bc_url}{lessonUrl}/master.m3u8?bcov_auth={stoken}"
+
+                        link = bc_url + lessonUrl + "/master.m3u8?bcov_auth=" + stoken
                         output_text += f"{lesson_name}: {link}\n"
+
                     elif lessonExt == 'youtube':
-                        link = f"https://www.youtube.com/embed/{lessonUrl}"
+                        link = "https://www.youtube.com/embed/" + lessonUrl
                         output_text += f"{lesson_name}: {link}\n"
+
                 except Exception as e:
-                    logging.error(f"Error processing lesson: {e}")
+                    print(f"Error processing lesson: {e}")
 
             with open(f"{name1}.txt", 'a') as f:
                 f.write(f"{output_text}")
@@ -63,13 +71,14 @@ async def careerdl(app, message, headers, raw_text2, class_id, notes_id, prog, n
 
     try:
         n_id = notes_id.split('&')
-        text = ""
+        for y in range(0, len(n_id)):
+            id = n_id[y]
 
-        for id in n_id:
-            details_url = f"https://elearn.crwilladmin.com/api/v5/batch-notes/{raw_text2}?topicId={id}"
+            details_url = "https://elearn.crwilladmin.com/api/v5/batch-notes/" + raw_text2 + "?topicId=" + id
             response = requests.get(details_url, headers=headers)
             notes_data = response.json()['data']['notesDetails']
 
+            text = ""
             for data in notes_data:
                 title = data['docTitle']
                 url = data['docUrl'].replace("\\/", "/")
@@ -91,14 +100,15 @@ async def careerdl(app, message, headers, raw_text2, class_id, notes_id, prog, n
     try:
         os.remove(f"{name1}.txt")
     except Exception as e:
-        logging.error(f"Error deleting file: {e}")
+        print(f"Error deleting file: {e}")
+
+
 
 async def career_will(app, message):
     try:
         input1 = await app.ask(message.chat.id, text="**Send ID & Password in this manner otherwise bot will not respond.\n\nSend like this:-  ID*Password\n\n OR Send Your Token**")
         login_url = "https://elearn.crwilladmin.com/api/v5/login-other"
         raw_text = input1.text
-
         if "*" in raw_text:
             headers = {
                 "Host": "elearn.crwilladmin.com",
@@ -150,28 +160,37 @@ async def career_will(app, message):
     await message.reply_text(f"**HERE IS YOUR BATCH**\n\n{FFF}")
     input2 = await app.ask(message.chat.id, text="**Now send the Batch ID to Download**")
     raw_text2 = input2.text
-    class_url = f"https://elearn.crwilladmin.com/api/v5/batch-topic/{raw_text2}?type=class"
+    class_url = "https://elearn.crwilladmin.com/api/v5/batch-topic/" + raw_text2 + "?type=class"
     response = requests.get(class_url, headers=headers)
     topic_data = response.json()
     class_data = topic_data['data']['batch_topic']
     name = topic_data["data"]["batch_detail"]["name"]
 
+#    BBB = "**TOPIC-ID - TOPIC**\n\n"
     class_id = ""
     for data in class_data:
         topic_id = data["id"]
+#        topic_name = data["topicName"]
         class_id += f"{topic_id}&"
+#        BBB += f"`{topic_id}` -  **{topic_name}** \n\n"
 
-    notes_url = f"https://elearn.crwilladmin.com/api/v5/batch-topic/{raw_text2}?type=notes"
+#    await message.reply_text(f"**Batches details of {name}**\n\n{BBB}")
+#    input3 = await app.ask(message.chat.id, text=f"Now send the **Topic IDs** to Download\n\nSend like this **1&2&3&4** so on\nor copy paste or edit **below ids** according to you :\n\n**Enter this to download full batch :-**\n`{id_num}`")
+#    raw_text3 = input3.text
+
+    notes_url = "https://elearn.crwilladmin.com/api/v5/batch-topic/" + raw_text2 + "?type=notes"
     r1 = requests.get(notes_url, headers=headers).json()
     notes_data = r1['data']['batch_topic']
     notes_id = ""
-    for data in notes_data:
+    for data in class_data:
         topic_id = data["id"]
         notes_id += f"{topic_id}&"
 
     prog = await message.reply_text("**Extracting Videos Links Please Wait  📥 **")
 
     try:
-        asyncio.create_task(careerdl(app, message, headers, raw_text2, class_id, notes_id, prog, name))
+        thread = threading.Thread(target=lambda: asyncio.run(careerdl(app, message, headers, raw_text2, class_id, notes_id, prog, name)))
+        thread.start()
+
     except Exception as e:
         await message.reply_text(str(e))
